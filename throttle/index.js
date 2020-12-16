552abef13883d54d9c23d836acb96c77234792d6
DalerAsrorov <asrorids@gmail.com>
2020-12-16 00:53:43 -0500

    Solution for throttle

---
+++ throttle/index.js
@@ -0,0 +1,29 @@
+function throttle(fn, ms) {
+    let lastCalled;
+    let timeout;
+    let ctx;
+    let args;
+
+    return function () {
+
+        if (!lastCalled || (new Date() - lastCalled > ms && !timeout)) {
+            lastCalled = new Date();
+            return fn.apply(this, arguments);
+        } else {
+
+            ctx = this;
+            args = [...arguments];
+
+            if (!timeout) {
+                timeout = setTimeout(function () {
+                    lastCalled = new Date();
+                    fn.apply(ctx, args);
+                    timeout = null;
+                }, ms);
+            }
+        }
+
+    };
+}
+
+module.exports = throttle;
